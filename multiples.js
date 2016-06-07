26f98079051a13fdca170e116272e16b27da1dcc
Sidney <user@SGD-MBP.local>
2016-06-07 09:34:28 -1000

    2nd Day Warm Up

--- multiples.js
+++ multiples.js
@@ -3,10 +3,21 @@
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
  * @return {Number}
  */
+
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
+      for(i=0;i<n;i++){
+      if(i%3===0 || i%5===0){
+        sum += i;
+      }
 
-  // do your work here
+      else{
 
+      }
+
+}
+  
+  console.log(sum);
   return sum;
+
 };
